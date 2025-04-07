6ec75d550208c91aa846b3f06ab24879a667c7d4
Robert S <berlamont@gmail.com>
2025-04-07 17:06:19 -0400

    feature-branch-2 - Add Syncfusion license and ARM template
    - Program.cs
      - Added Syncfusion license registration line.
    
    - profile.arm.json
      - Introduced a new Azure Resource Manager (ARM) template for resource deployment, including resource group and App Service configurations.

--- miwebmedia.Client/Program.cs
+++ miwebmedia.Client/Program.cs
@@ -3,7 +3,7 @@ using miwebmedia.Client;
 using Syncfusion.Blazor;
 
 //Register Syncfusion license https://help.syncfusion.com/common/essential-studio/licensing/how-to-generate
-//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR LICENSE KEY");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXted3VQQmFdVUVwV0pWYUA=");
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddSyncfusionBlazor();
 await builder.Build().RunAsync();
